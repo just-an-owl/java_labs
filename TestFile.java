508f286498c9e7ec5cacbb2f68578876f946614d
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2020-10-19 01:25:17 +0400

    some change

--- TestFile.java
+++ TestFile.java
@@ -3,6 +3,6 @@ import Functions.*;
 public class TestFile {
     public static void main(String[] arg){
         LinkedListTabulatedFunction testTabFun = new LinkedListTabulatedFunction(1,15,5);
-
+        System.out.println(testTabFun.getPoint(0).getX());
     }
 }
