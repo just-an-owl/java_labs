320500c0b48b92c99106f78cbc30c73876f1520a
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2020-10-28 20:51:17 +0300

    some change

--- Functions/TabulatedFunctions.java
+++ Functions/TabulatedFunctions.java
@@ -1,5 +1,7 @@
 package functions;
 
+import java.io.*;
+
 public abstract class TabulatedFunctions {
     public static TabulatedFunction tabule(Function f, double leftX, double rightX, int pointsCount) throws InappropriateFunctionPointException {
         if (f.getRightDomainBorder()<rightX || f.getLeftDomainBorder()>leftX) throw new IllegalArgumentException("invalid border");
@@ -8,6 +10,38 @@ public abstract class TabulatedFunctions {
         for (int i=0; i<pointsCount;++i){
             points[i].set(leftX+step+step*i, f.getFunctionValue(leftX+step+step*i));
         }
+        return new LinkedListTabulatedFunction(leftX,rightX, points);
+    }
+
+    public static void outputTabulatedFunction(TabulatedFunction f, OutputStream out) throws IOException {
+        DataOutputStream stream = new DataOutputStream(out);
+
+        stream.write(f.getPointCount());
+        stream.writeDouble(f.getLeftDomainBorder());
+
+        stream.writeDouble(f.getRightDomainBorder());
+
+        for (int i = 0; i<f.getPointCount(); ++i){
+            stream.writeDouble(f.getPointX(i));
+            stream.writeDouble(f.getPointY(i));
+        }
+    }
+
+    public static TabulatedFunction inputTabulatedStream(InputStream in) throws IOException {
+        DataInputStream inputStream = new DataInputStream(in);
+        int count;
+        count = inputStream.readInt();
+        FunctionPoint[] points = new FunctionPoint[count];
+        double x;
+        double y;
+        double leftX = inputStream.readDouble();
+        double rightX = inputStream.readDouble();
+        for (int i = 0; i<count; ++i){
+            x=inputStream.readDouble();
+            y=inputStream.readDouble();
+            points[i]=new FunctionPoint(x,y);
+        }
+//jdkdk
         return new LinkedListTabulatedFunction(leftX,rightX, points);
     }
 }
