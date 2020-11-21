64065a70dd79ffab6f1d28400ab2f0e8d3138620
just-an-owl <55064514+just-an-owl@users.noreply.github.com>
2020-11-21 22:14:00 +0400

    some change

--- MainClass.java
+++ MainClass.java
@@ -1,32 +1,46 @@
 import functions.*;
 
 public class MainClass{
-    public static void main(String[] arg) throws InappropriateFunctionPointException {
-        ArrayTabulatedFunction arrayTabulatedFunction = null;
-        try {
-            arrayTabulatedFunction = new ArrayTabulatedFunction(1, 15, -5);
+    public static void main(String[] arg) throws InappropriateFunctionPointException, CloneNotSupportedException {
+        double[] arr = {-1, 0, 1};
+        TabulatedFunction tab1, tab2, tab1a, tab2a;
+        System.out.println("Массив");
+        tab1 = new ArrayTabulatedFunction(0, 2, arr);
+        tab1a = new ArrayTabulatedFunction(0, 2, arr);
+        for (int i = 0; i < tab1.getPointCount(); i++) {
+            System.out.println("Значение в точке x=" + i + ": " + tab1.getFunctionValue(i));
         }
-        catch (InappropriateFunctionPointException fail){
-            System.out.println(fail.getMessage());
+        System.out.println(tab1.toString());
+        System.out.println("Список");
+        tab2 = new LinkedListTabulatedFunction(0, 2, arr);
+        tab2a = new LinkedListTabulatedFunction(0, 2, arr);
+        for (int i = 0; i < tab1.getPointCount(); i++) {
+            System.out.println("Значение в точке x=" + i + ": " + tab2.getFunctionValue(i));
         }
-        try {
-            arrayTabulatedFunction = new ArrayTabulatedFunction(1, 15, 5);
+        System.out.println(tab2.toString());
+        if (tab2a.equals(tab2)) {
+            System.out.println("Равны");
+        } else {
+            System.out.println("Не равны");
         }
-        catch (InappropriateFunctionPointException fail){
-            System.out.println(fail.getMessage());
-        }
-        TabulatedFunction tabulatedFunction = arrayTabulatedFunction;
-        TabulatedFunction[] tabulatedFunctions = new TabulatedFunction[2];
-        tabulatedFunctions[0] = tabulatedFunction;
-        tabulatedFunctions[1] = new LinkedListTabulatedFunction(1,15,5);
-        FunctionPoint point = new FunctionPoint(2, 3);
-        tabulatedFunctions[0].setPoint(0, point);
-        tabulatedFunctions[1].setPoint(0, point);
-        point.set(5, 7);
-        tabulatedFunctions[0].setPoint(1, point);
-        tabulatedFunctions[1].setPoint(1, point);
-        for(int i = 0; i<2; ++i){
-            System.out.println(tabulatedFunctions[i].getFunctionValue(4));
-        }
+        System.out.println(tab1.hashCode());
+        System.out.println(tab1a.hashCode());
+        System.out.println(tab2.hashCode());
+        System.out.println(tab2a.hashCode());
+        tab1.setPointX(1, 0.99);
+        System.out.println(tab1.hashCode());
+        System.out.println("Клонирование объектов");
+        TabulatedFunction tab1c, tab2c;
+        System.out.println(tab1.toString());
+        System.out.println(tab2.toString());
+        tab2c = (LinkedListTabulatedFunction) tab2.clone();
+        tab1c = (ArrayTabulatedFunction) tab1.clone();
+        tab1.setPointX(1, 0.5);
+        System.out.println(tab1.toString());
+        System.out.println(tab1c.toString());
+        System.out.println(tab2.toString());
+        tab2.setPointX(1, 0.5);
+        System.out.println(tab2.toString());
+        System.out.println(tab2c.toString());
     }
 }
